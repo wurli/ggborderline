4ad4e9233352ef709cd0df19bdf9b8c1e1fbef6b
Jacob Scott <jacob.scott@education.gov.uk>
2022-10-11 11:35:49 +0100

    define message_wrap

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,10 @@
 `%||%` <- function(x, y) {
   if (is.null(x)) y else x
 }
+
+# Taken from ggplot2
+message_wrap <- function(...) {
+  msg <- paste(..., collapse = "", sep = "")
+  wrapped <- strwrap(msg, width = getOption("width") - 2)
+  message(paste0(wrapped, collapse = "\n"))
+}
